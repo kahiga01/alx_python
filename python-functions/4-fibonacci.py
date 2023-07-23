a1ab0c228f7ceac43105779bd57efbf5693b45b2
Kahiga01 <danielkahiga69@gmail.com>
2023-07-23 11:44:09 -0700

    4-fibonacci.py

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci_sequence(n):
+    fibonacci_numbers = [0, 1]
+    while len(fibonacci_numbers) < n:
+        next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
+        fibonacci_numbers.append(next_number)
+    return fibonacci_numbers[:n]
+
+n = 10
+fibonacci_numbers = fibonacci_sequence(n)
