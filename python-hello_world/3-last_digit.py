566057cc7922a8c00c4a867949472febf5a0eb9d
Kahiga01 <danielkahiga69@gmail.com>
2023-07-23 09:51:28 -0700

    3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
@@ -10,15 +11,4 @@ elif last_digit == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
-
-
-
-
-
-
-
-
-
-
-
 
