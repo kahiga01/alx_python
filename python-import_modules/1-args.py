1f7b1a97583aa7497e970c25dd42f12d278118b2
Kahiga01 <danielkahiga69@gmail.com>
2023-07-24 12:44:19 -0700

    1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,19 +1,17 @@
+import sys
+
 if __name__ == "__main__":
-    import sys
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
 
-    # Number of arguments
-    num_args = len(sys.argv) - 1
-
-    # Output the number of arguments
-    if num_args == 0:
-        print("0 arguments.")
-    elif num_args == 1:
-        print("1 argument:")
+    print("Number of argument(s):", num_arguments, end=" ")
+    if num_arguments == 0:
+        print(".")
+    elif num_arguments == 1:
+        print("argument:")
     else:
-        print("{} arguments:".format(num_args))
+        print("arguments:")
 
-    # Output the list of arguments
-    if num_args > 0:
-        for i in range(1, num_args + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+    for i, arg in enumerate(arguments, 1):
+        print("{}: {}".format(i, arg))
 
