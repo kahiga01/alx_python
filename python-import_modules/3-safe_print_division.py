e4fdccfd508cfb28a216d5fdb8de55f77d9e218c
Kahiga01 <danielkahiga69@gmail.com>
2023-07-24 13:03:53 -0700

    3-safe_print_division.py

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,14 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Division by zero is not allowed.")
+        return None
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result
+
