7b8199dcc4a530b37fda820f59677dfd6fe48c7e
Kahiga01 <danielkahiga69@gmail.com>
2023-07-24 12:28:07 -0700

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    # declare two variable in different line
-    a = 1
-    b = 2
+    # declare two variables in the same line
+    a, b = 1, 2
 
     # import module add
     from add_0 import add
