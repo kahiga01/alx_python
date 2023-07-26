da5ae7d71495a5dffacd36460cebe85feaf2b8a0
Kahiga01 <danielkahiga69@gmail.com>
2023-07-26 10:43:02 -0700

    1-print_matrix_integer.py

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,8 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            if i != 0:
+                print(" ", end="")
+            print("{:d}".format(row[i]), end="")
+        print()
+
