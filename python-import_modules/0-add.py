8882163ffd44318ac3437d587f4f90a434b0bc4e
Kahiga01 <danielkahiga69@gmail.com>
2023-07-26 10:15:02 -0700

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,10 +3,9 @@
 a = 1
 b = 2
 
-# Define the add function inline without importing add_0.py
 def add(a, b):
-    return (a + b)
+    return a - b
 
 result = add(a, b)
-print(f"{a} + {b} = {result}")
+print(f"FAKE add() => {a} - {b} = {result}")
 
