08af12a3c498b5cd7f3f450c8d29fd035ccddca6
Kahiga01 <danielkahiga69@gmail.com>
2023-07-26 10:53:29 -0700

    2-multiple_returns.py

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -3,12 +3,3 @@ def multiple_returns(sentence):
     first_char = sentence[0] if length > 0 else None
     return length, first_char
 
-# Test the function
-sentence1 = "Hello, World!"
-result1 = multiple_returns(sentence1)
-print(result1)  # Output: (13, 'H')
-
-sentence2 = ""
-result2 = multiple_returns(sentence2)
-print(result2)  # Output: (0, None)
-
