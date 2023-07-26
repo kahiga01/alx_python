51ac597d3b08133f53e605cf46cc076a57594e16
Kahiga01 <danielkahiga69@gmail.com>
2023-07-26 10:58:33 -0700

    0-no_c.py

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,12 +1,7 @@
 def no_c(my_string):
     result = ''
     for char in my_string:
-        if char.lower() != 'c':
+        if char not in 'cC':
             result += char
     return result
-
-# Test the function
-input_string = "Can you C me now?"
-output_string = no_c(input_string)
-print(output_string)  # Output: "an you  me now?"
 
