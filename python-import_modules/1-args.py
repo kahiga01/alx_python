c3795674f416b503b3115bc593d6b9d102a4dd40
Kahiga01 <danielkahiga69@gmail.com>
2023-07-26 10:19:31 -0700

    1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,29 +1,23 @@
+# number_and_list_of_args.py
+
 import sys
 
-# Get the number of arguments passed to the script
-num_args = len(sys.argv) - 1
+def print_arguments(arguments):
+    num_arguments = len(arguments)
+    print(f"Number of argument(s): {num_arguments}", end="")
+    
+    if num_arguments == 0:
+        print(":", end="")
+    elif num_arguments == 1:
+        print(", followed by:", end="")
+    else:
+        print("s, followed by:", end="")
+    
+    print()
 
-# Print the number of arguments and the list of arguments
-print(f"Number of argument{'s' if num_args != 1 else ''}: {num_args}{'' if num_args == 0 else ':'}")
-for i, arg in enumerate(sys.argv[1:], start=1):
-    print(f"{i}: {arg}")
-
-# If no arguments were passed, print a dot
-if num_args == 0:
-    print(".")
-import sys
+    for idx, arg in enumerate(arguments, start=1):
+        print(f"{idx}: {arg}")
 
 if __name__ == "__main__":
-    arguments = sys.argv[1:]
-    num_arguments = len(arguments)
+    print_arguments(sys.argv[1:])
 
-    print("Number of argument(s):", num_arguments, end=" ")
-    if num_arguments == 0:
-        print(".")
-    elif num_arguments == 1:
-        print("argument:")
-    else:
-        print("arguments:")
-
-    for i, arg in enumerate(arguments, 1):
-        print("{}: {}".format(i, arg))
