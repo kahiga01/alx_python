7e705aac3d2a2475ab748b8c1198517099082473
Kahiga01 <danielkahiga69@gmail.com>
2023-07-26 06:14:42 -0700

    2-variable_load.py

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -3,6 +3,5 @@
 if __name__ == "__main__":
     try:
         from variable_load_2 import a
-        print("Correct output - case: a =", a)
-    except ImportError:
-        print("Correct output - case: a missing")
+        print(a)
+
