1a7f4454c678addcbbc9f0f20123e3826c2c9b6e
Kahiga01 <danielkahiga69@gmail.com>
2023-07-26 12:01:34 -0700

    3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,9 +4,9 @@ import random
 number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
-is_greater_than_5 = "and is greater than 5" if last_digit > 5 else ""
-is_zero = "and is 0" if last_digit == 0 else ""
-is_less_than_6 = "and is less than 6 and not 0" if last_digit < 6 and last_digit != 0 else ""
+is_positive = "positive" if number > 0 else ""
+is_negative = "negative" if number < 0 else ""
+is_zero = "zero" if number == 0 else ""
 
-print("The string Last digit of", number, "is", last_digit, is_greater_than_5, is_zero, is_less_than_6)
+print("The string Last digit of", number, "is", last_digit, "and is", is_positive or is_negative or is_zero)
 
