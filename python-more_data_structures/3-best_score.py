4ab6e40fbfc445a00b8fed28899e65681856781e
Kahiga01 <danielkahiga69@gmail.com>
2023-07-27 11:19:38 -0700

    3-best_score.py

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,12 +1,14 @@
 def best_score(a_dictionary):
-    # Create variables to keep track of the current highest score and its corresponding key.
+    if not isinstance(a_dictionary, dict) or not a_dictionary:
+        return None
+
     max_score = None
     max_key = None
 
-    # Iterate through the dictionary to locate the key associated with the highest integer value.
     for key, value in a_dictionary.items():
         if max_score is None or value > max_score:
             max_score = value
             max_key = key
 
     return max_key
+
