f8e896dbd8116c3cf544d2815ce7a8d5536ca6b9
Kahiga01 <danielkahiga69@gmail.com>
2023-07-27 11:51:21 -0700

    1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,9 +4,9 @@ def print_arguments(args):
     num_args = len(args)
     plural_suffix = 's' if num_args != 1 else ''
     argument_str = 'argument' if num_args == 1 else 'arguments'
-    
+
     print(f"Number of argument{plural_suffix}: {num_args}, followed by:", end="")
-    
+
     if num_args == 0:
         print(" .")
     else:
