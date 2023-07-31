e71810725892b9080322c719af762de07018cdd8
Kahiga01 <danielkahiga69@gmail.com>
2023-07-31 12:02:39 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 class Square:
     """
-    The size of the square is a private instance attribute for this class.
-    Since the square's dimensions are important for many calculations, they are kept secret.
+    This class represents a square with a private instance attribute size.
+    The size of the square is crucial for various computations, and thus it's kept private.
     """
 
     def __init__(self, size):
@@ -13,4 +13,41 @@ class Square:
         """
         self.__size = size
 
+    def dict_(self):
+        """
+        Returns an empty dictionary.
+
+        Returns:
+            dict: An empty dictionary.
+        """
+        return {}
+
+# Output Case 1
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.dict_())
+
+# Output Case 2
+mysquare = Square(89)
+print(type(mysquare))
+print(mysquare.dict_())
+
+# Output Case 3
+try:
+    print(mysquare.size)
+except AttributeError as e:
+    print(e)
+
+# Output Case 4
+try:
+    print(mysquare._size)
+except AttributeError as e:
+    print(e)
+
+# PEP8 Validation
+import pep8
+
+style = pep8.StyleGuide(quiet=True)
+result = style.check_files(["your_file.py"])
+print(result.total_errors)
 
