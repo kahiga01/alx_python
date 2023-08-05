dbb983abef7771cf7460c3576d82433d7f8af7df
Kahiga01 <danielkahiga69@gmail.com>
2023-08-05 10:21:37 -0700

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,15 +4,14 @@ class Square:
 
     Attributes:
         __size (int): The size of the square.
-
     """
 
     def __init__(self, size=0):
         """
-        Initializes a Square instance with a given size.
+        Constructor to initialize the Square with an optional size.
 
         Args:
-            size (int): The size of the square (default is 0).
+            size (int, optional): The size of the square. Defaults to 0.
 
         Raises:
             TypeError: If size is not an integer.
@@ -20,18 +19,6 @@ class Square:
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
         if size < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = size
-
-    def area(self):
-        """
-        Calculates the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size * self.__size
-
