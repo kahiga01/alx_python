ac4443d8a9aee44be36d3e311983af9fd9633983
Kahiga01 <danielkahiga69@gmail.com>
2023-08-06 10:46:35 -0700

    0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -13,26 +13,26 @@ def is_same_class(obj, a_class):
 
 # Test cases
 a = 1
-print(is_same_class(a, int))  # Output: True
+print(is_same_class(a, int))  # Correct output: True
 
 a = True
-print(is_same_class(a, int))  # Output: False
+print(is_same_class(a, int))  # Correct output: False
 
 a = 3.14
-print(is_same_class(a, int))  # Output: False
+print(is_same_class(a, int))  # Correct output: False
 
 a = True
-print(is_same_class(a, object))  # Output: True
+print(is_same_class(a, object))  # Correct output: True
 
 a = None
-print(is_same_class(a, object))  # Output: True
+print(is_same_class(a, object))  # Correct output: True
 
 a = None
-print(is_same_class(a, list))  # Output: False
+print(is_same_class(a, list))  # Correct output: False
 
 a = [1, 2, 3]
-print(is_same_class(a, list))  # Output: True
+print(is_same_class(a, list))  # Correct output: True
 
 a = [1, 2, 3]
-print(is_same_class(a, object))  # Output: False
+print(is_same_class(a, object))  # Correct output: False
 
