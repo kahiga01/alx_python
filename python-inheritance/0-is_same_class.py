4d1483ce90700cfdeccefb7172e89ba2b91c3abd
Kahiga01 <danielkahiga69@gmail.com>
2023-08-06 11:31:02 -0700

    0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,38 +1,12 @@
 def is_same_class(obj, a_class):
-    """
-    Check if the given object is exactly an instance of the specified class.
+    """Check if the object is exactly an instance of the specified class.
 
-    :param obj: The object to check.
-    :type obj: Any
-    :param a_class: The class to compare with.
-    :type a_class: type
-    :return: True if obj is exactly an instance of a_class, otherwise False.
-    :rtype: bool
+    Args:
+        obj: The object to be checked.
+        a_class: The class to compare the object against.
+
+    Returns:
+        True if the object is exactly an instance of the specified class, otherwise False.
     """
     return type(obj) is a_class
-
-# Test cases
-a = 1
-print(is_same_class(a, int))  # Correct output: True
-
-a = True
-print(is_same_class(a, int))  # Correct output: False
-
-a = 3.14
-print(is_same_class(a, int))  # Correct output: False
-
-a = True
-print(is_same_class(a, object))  # Correct output: True
-
-a = None
-print(is_same_class(a, object))  # Correct output: True
-
-a = None
-print(is_same_class(a, list))  # Correct output: False
-
-a = [1, 2, 3]
-print(is_same_class(a, list))  # Correct output: True
-
-a = [1, 2, 3]
-print(is_same_class(a, object))  # Correct output: False
 
