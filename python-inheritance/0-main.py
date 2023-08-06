41e5aeca39c0511eae66af388690f30d693d1fd2
Kahiga01 <danielkahiga69@gmail.com>
2023-08-06 12:00:00 -0700

    0-main.py

--- python-inheritance/0-main.py
+++ python-inheritance/0-main.py
@@ -1,6 +1,6 @@
+
 #!/usr/bin/python3
-def is_same_class(obj, a_class):
-    return type(obj) is a_class
+is_same_class = __import__("0-is_same_class").is_same_class
 
 a = 1
 if is_same_class(a, int):
@@ -9,4 +9,3 @@ if is_same_class(a, float):
     print("{} is an instance of the class {}".format(a, float.__name__))
 if is_same_class(a, object):
     print("{} is an instance of the class {}".format(a, object.__name__))
-
