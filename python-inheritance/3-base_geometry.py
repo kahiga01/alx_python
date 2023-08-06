882d76c1dee58d3ed5026407f48ec6aa446842a2
Kahiga01 <danielkahiga69@gmail.com>
2023-08-06 12:15:09 -0700

    3-base_geometry.py

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+A basic template for representing geometric entities.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty base class to represent geometric entities.
+
+    This class acts as a foundation for other classes to inherit from, allowing them
+    to define specific geometric entities and their corresponding behavior.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+
+    pass
+
