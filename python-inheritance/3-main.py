dbced654dd8bc893732983eb774f14c46ad352d8
Kahiga01 <danielkahiga69@gmail.com>
2023-08-06 12:18:35 -0700

    3-main.py

---
+++ python-inheritance/3-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Import the BaseGeometry class from the module 3-base_geometry
+from 3-base_geometry import BaseGeometry
+
+# Create an instance of the BaseGeometry class
+bg = BaseGeometry()
+
+# Print the instance
+print(bg)
+
+# Print the list of attributes and methods available for the instance
+print(dir(bg))
+
+# Print the list of attributes and methods available for the BaseGeometry class
+print(dir(BaseGeometry))
+
