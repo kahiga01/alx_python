f181d3e4b906f174f8e6c964de6d434389874d07
Kahiga01 <danielkahiga69@gmail.com>
2023-08-08 10:05:34 -0700

    1-main.py

--- python-classes/1-main.py
+++ python-classes/1-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
+Square = __import__("1-square").Square
 
 my_square_1 = Square(3)
 print(type(my_square_1))
@@ -32,4 +32,3 @@ try:
     print(my_square_4.__dict__)
 except Exception as e:
     print(e)
-
