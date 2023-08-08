e1e1ae373ff95acb0355b8ab7122db24efed2e69
Kahiga01 <danielkahiga69@gmail.com>
2023-08-08 11:30:04 -0700

    Improve Geometry

--- python-inheritance/4-main.py
+++ python-inheritance/4-main.py
@@ -1,33 +1,9 @@
-from 3-base_geometry import BaseGeometry
+#!/usr/bin/python3
+BaseGeometry = __import__('4-base_geometry').BaseGeometry
 
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+bg = BaseGeometry()
 
-    def area(self):
-        return self.width * self.height
-
-class Square(BaseGeometry):
-    def __init__(self, side):
-        self.side = side
-
-    def area(self):
-        return self.side * self.side
-
-if __name__ == "__main__":
-    # Create instances of the derived classes
-    rectangle = Rectangle(5, 4)
-    square = Square(3)
-
-    # Calculate and print areas
-    print("Rectangle area:", rectangle.area())
-    print("Square area:", square.area())
-
-    # Calling the area() method on the BaseGeometry directly will raise an Exception
-    try:
-        base = BaseGeometry()
-        base.area()
-    except Exception as e:
-        print("Exception:", e)
-
+try:
+    print(bg.area())
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
