c19c607e1609d295bb8e7c2220023b4ba5eacdd0
Kahiga01 <danielkahiga69@gmail.com>
2023-08-08 10:43:01 -0700

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,28 +1,44 @@
+# -*- coding: utf-8 -*-
+"""Square module.
+
+This module defines the Square class, which represents a square with a private size attribute.
+"""
+
+
 class Square:
-    """
-    A class representing a square.
+    """Represents a square with a private size attribute.
 
     Attributes:
-        __size (int): The size of the square (private attribute).
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initialize the Square instance with an optional size.
+        area(self): Calculate the area of the square.
+        my_print(self): Print the square with the character '#'.
+
+    Properties:
+        size (int): Getter and setter for the size attribute.
+
+    Raises:
+        TypeError: If the size is not an integer.
+        ValueError: If the size is less than 0.
     """
 
     def __init__(self, size=0):
-        """
-        Initializes a new Square instance.
+        """Initialize the Square instance with an optional size.
 
         Args:
-            size (int): The size of the square (default is 0).
-
+            size (int, optional): The size of the square. Defaults to 0.
+        
         Raises:
-            TypeError: If the provided size is not an integer.
-            ValueError: If the provided size is less than 0.
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than 0.
         """
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
-        """
-        Get the size of the square.
+        """Getter method to retrieve the size of the square.
 
         Returns:
             int: The size of the square.
@@ -31,26 +47,23 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """
-        Set the size of the square.
+        """Setter method to set the size of the square.
 
         Args:
-            value (int): The size of the square.
+            value (int): The new size value.
 
         Raises:
-            TypeError: If the provided size is not an integer.
-            ValueError: If the provided size is less than 0.
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than 0.
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     def area(self):
-        """
-        Calculate the area of the square.
+        """Calculate the area of the square.
 
         Returns:
             int: The area of the square.
@@ -58,14 +71,14 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """
-        Print the square pattern using the '#' character.
+        """Print the square with '#' characters.
 
-        If size is equal to 0, print an empty line.
+        If the size is equal to 0, print an empty line.
         """
         if self.__size == 0:
             print()
-        else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
- 
+            return
+
+        for _ in range(self.__size):
+            print("#" * self.__size)
+
