2d765769f7244c4064867640ff176613f0d8d6f9
Kahiga01 <danielkahiga69@gmail.com>
2023-08-08 11:08:29 -0700

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,13 +2,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Division by zero is not allowed.")
-        return None
-    except Exception as e:
-        print("An error occurred:", e)
-        return None
+        result = None
     finally:
-        print("Inside result: {}".format(result))
-
-    return result
-
+        print("Inside result: {}".format(str(result)))
+        return result
