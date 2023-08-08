45420e1e93db300f251786b94fc39340fbcdc694
Kahiga01 <danielkahiga69@gmail.com>
2023-08-08 10:00:03 -0700

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,64 +1,34 @@
+"""
+A class representing a square.
+"""
+
+
 class Square:
     """
-    This class represents a square.
+    A class representing a square.
 
     Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        __init__(self, size=0): Initializes the Square instance.
+        __size (int): The size of the square (private attribute).
     """
 
     def __init__(self, size=0):
         """
-        Initializes a Square instance.
+        Initializes a new Square instance.
 
         Args:
-            size (int): The size of the square (default 0).
+            size (int): The size of the square (default is 0).
 
         Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
+            TypeError: If the provided size is not an integer.
+            ValueError: If the provided size is less than 0.
+
+        Returns:
+            None.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-
-        self.__size = size
-
-
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print(type(my_square_1))
-    print(my_square_1.__dict__)
-
-    my_square_2 = Square()
-    print(type(my_square_2))
-    print(my_square_2.__dict__)
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_3 = Square("3")
-        print(type(my_square_3))
-        print(my_square_3.__dict__)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_4 = Square(-89)
-        print(type(my_square_4))
-        print(my_square_4.__dict__)
-    except Exception as e:
-        print(e)
+        else:
+            self.__size = size
 
