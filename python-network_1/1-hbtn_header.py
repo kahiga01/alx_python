abe2192fd377f1d87c478dc538454e54b2bb5b62
Kahiga01 <danielkahiga69@gmail.com>
2023-08-12 11:50:59 -0700

    Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""This script accepts a URL,
+"""A script that takes in a URL,
 Sends a request to the URL,
-And extracts and displays the value of the X-Request-Id header from the response.
+And displays the value of the X-Request-Id variable found in the header ofthe response.
 """
 
 import requests
 import sys
 
-def extract_x_request_id(url):
-    """Retrieves the value of the X-Request-Id header from the response to the specified URL."""
+
+def get_x_request_id(url):
+    """Gets the value of the X-Request-Id header from the response to the given URL."""
     response = requests.get(url)
     return response.headers["X-Request-Id"]
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
-    print(extract_x_request_id(url))
+    print(get_x_request_id(url))
 
