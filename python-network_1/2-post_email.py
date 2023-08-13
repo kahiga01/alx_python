9be02e45de7fa43a41cf251076d2e35a3360790e
Kahiga01 <danielkahiga69@gmail.com>
2023-08-12 23:21:44 -0700

    2. POST an email #1

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
 """
-import sys
-import urllib.parse
-import urllib.request
+Sends a POST request to the passed URL with the email as a parameter and displays the body of the response.
+"""
 
+import requests
+import sys
+
+def send_post_request(url, email):
+    """
+    Sends a POST request to the specified URL with the email as a parameter
+    and displays the body of the response.
+
+    Args:
+        url (str): The URL to send the request to.
+        email (str): The email address to be sent as a parameter.
+
+    Returns:
+        None
+    """
+    payload = {"email": email}
+    response = requests.post(url, data=payload)
+
+    print(response.text)
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    email = sys.argv[2]
+    send_post_request(url, email)
 
