ff1e7b6ff33853c0fb4dbed5a5b88b91f4e00c54
Kahiga01 <danielkahiga69@gmail.com>
2023-08-12 22:57:31 -0700

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,27 +1,22 @@
-#!/usr/bin/python3
-"""A script that retrieves the information from https://intranet.hbtn.io/status and provides details about the response."""
+#!/usr/bin/env python3
+"""
+Module fetches and dispies the status of the website.
+"""
+
 import requests
 
-def fetch_and_display_status(url):
+def fetch_hbtn_status():
     """
-    Retrieves content from the specified URL and provides information about the response.
-
-    Args:
-        url (str): The URL from which content is to be retrieved.
-
-    Returns:
-        None
+    Fetches and displays the status of https://alu-intranet.hbtn.io/status.
     """
+    url = 'https://alu-intranet.hbtn.io/status'
     response = requests.get(url)
-
-    if response.status_code == 200:
-        print("Response body:")
-        print("\t- Data type:", type(response.text))
-        print("\t- Content:", response.text.strip())
-    else:
-        print("Failed to retrieve the URL. Status code:", response.status_code)
-
+    content_type = type(response.text)
+    content = response.text
+   
+    print("Body response:")
+    print(f"\t- type: {content_type}")
+    print(f"\t- content: {content}")
+   
 if __name__ == "__main__":
-    target_url = "https://alu-intranet.hbtn.io/status"
-    fetch_and_display_status(target_url)
-
+    fetch_hbtn_status()
--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""
-A script that takes a URL as input, sends a request to the URL,
-and displays the value of the variable X-Request-Id from the response header.
+"""A script that takes in a URL,
+Sends a request to the URL,
+And displays the value of the X-Request-Id variable found in the header ofthe response.
 """
 
 import requests
 import sys
 
+
 def get_x_request_id(url):
-    """
-    Sends a request to the specified URL and displays the value of the X-Request-Id from the response header.
-
-    Args:
-        url (str): The URL to send the request to.
-
-    Returns:
-        None
-    """
+    """Gets the value of the X-Request-Id header from the response to the given URL."""
     response = requests.get(url)
-    x_request_id = response.headers.get("X-Request-Id")
-    
-    if x_request_id is not None:
-        print("X-Request-Id:", x_request_id)
-    else:
-        print("X-Request-Id header not found in the response.")
+    return response.headers["X-Request-Id"]
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    get_x_request_id(url)
-
+    print(get_x_request_id(url))
