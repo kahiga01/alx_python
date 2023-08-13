c1db401d1ad3a770e6c856de40e4dff87db57608
Kahiga01 <danielkahiga69@gmail.com>
2023-08-12 23:12:32 -0700

    2. POST an email #1

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Sends a POST request to the passed URL with the email as a parameter,
+Sends a GET request to the passed URL with the email as a query parameter,
 and displays the body of the response (decoded in utf-8)
 """
 
@@ -9,11 +9,16 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    email = "test@test.com"
 
-    response = requests.get(url)
+    # Construct the full URL with the email as a query parameter
+    full_url = f"{url}?email={email}"
+
+    response = requests.get(full_url)
 
     if response.status_code >= 400:
         print("Error code:", response.status_code)
     else:
+        print("Email:", email)
         print(response.text)
 
