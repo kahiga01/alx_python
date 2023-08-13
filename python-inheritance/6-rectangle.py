f8dd14003664e9ee45e50146e5f15fdae8f7fccf
Kahiga01 <danielkahiga69@gmail.com>
2023-08-12 23:42:22 -0700

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,55 +1,28 @@
 #!/usr/bin/python3
+"""
+Creates a Rectangle class.
+"""
 
-"""A class representing a rectangle, inheriting from BaseGeometry."""
 
-BaseGeometry = __import__("5-base_geometry").BaseGeometry
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    A class representing a rectangle, inheriting from BaseGeometry.
-
-    This class inherits from the BaseGeometry class and provides functionality to represent a rectangle.
-    It includes validation for positive integer values for width and height.
-
-    Attributes:
-        __width (int): The width of the rectangle (private attribute).
-        __height (int): The height of the rectangle (private attribute).
-
-    Methods:
-        __init__(self, width, height):
-            Initializes a new Rectangle object with the given width and height.
-
-        __str__(self):
-            Returns a string representation of the Rectangle in the format [Rectangle] width/height.
-
+    """class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
+    Private instance attributes:
+        - width
+        - height
+    Inherits from BaseGeometry.
     """
 
     def __init__(self, width, height):
-        """
-        Initializes a new Rectangle object with the given width and height.
-
+        """Initializes an instance.
         Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If either width or height is not an integer.
-            ValueError: If either width or height is not a positive integer.
-
+            - width: width of the rectangle
+            - heigth: height of the rectangle
         """
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
-    def __str__(self):
-        """
-        Returns a string representation of the Rectangle in the format [Rectangle] width/height.
-
-        Returns:
-            str: The string representation of the Rectangle object.
-
-        """
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        self.__width = width
+        self.__height = height
 
