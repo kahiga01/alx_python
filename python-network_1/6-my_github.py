6310fca6311505169f34f2915af2c024daeffffd
Kahiga01 <danielkahiga69@gmail.com>
2023-08-12 23:14:44 -0700

    5. My GitHub!

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,35 +1,13 @@
 #!/usr/bin/python3
-"""
-A script that uses Basic Authentication with a personal access token to access the GitHub API
-and display your GitHub user ID.
-"""
+"""takes your GitHub credentials (username and password)
+   uses the GitHub API to display your id"""
+from sys import argv
+from requests import get
+from requests.auth import HTTPBasicAuth
 
-import requests
-import sys
-
-def get_github_user_id(username, token):
-    """
-    Retrieves your GitHub user ID using Basic Authentication with a personal access token.
-
-    Args:
-        username (str): Your GitHub username.
-        token (str): Your personal access token.
-
-    Returns:
-        None
-    """
-    url = "https://api.github.com/user"
-    response = requests.get(url, auth=(username, token))
-
-    if response.status_code == 200:
-        user_data = response.json()
-        user_id = user_data["id"]
-        print("Your GitHub user ID is:", user_id)
-    else:
-        print("Failed to retrieve user information. Status code:", response.status_code)
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    token = sys.argv[2]
-    get_github_user_id(username, token)
+    credentials = HTTPBasicAuth(argv[1], argv[2])
+    request = get("https://api.github.com/user", auth=credentials)
+    print(request.json().get("id"))
 
