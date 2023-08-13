51f59eee797695602fd2f8035b2950923f2ddb34
Kahiga01 <danielkahiga69@gmail.com>
2023-08-12 23:01:48 -0700

    1. Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """A script that takes in a URL,
 Sends a request to the URL,
-And displays the value of the X-Request-Id variable found in the header ofthe response.
-"""
+And displays the value of the X-Request-Id variable found in the header ofthe response"""
 
 import requests
 import sys
 
+def main():
+    """Main function to fetch and display the X-Request-Id header"""
+    if len(sys.argv) != 2:
+        print("Usage: ./1-hbtn_header.py <URL>")
+        sys.exit(1)
 
-def get_x_request_id(url):
-    """Gets the value of the X-Request-Id header from the response to the given URL."""
+    url = sys.argv[1]
     response = requests.get(url)
-    return response.headers["X-Request-Id"]
+    x_request_id = response.headers.get('X-Request-Id')
 
+    if x_request_id is not None:
+        print(x_request_id)
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    print(get_x_request_id(url))
+    main()
