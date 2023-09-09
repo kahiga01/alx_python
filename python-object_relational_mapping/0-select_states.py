ea53b67925f546c2f43050aeba1227254146abe5
Kahiga01 <danielkahiga69@gmail.com>
2023-09-09 09:37:49 -0700

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,57 +1,41 @@
 #!/usr/bin/python3
+import MySQLdb
+import sys
 
-# 0. Get all states
-# mandatory
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-# Write a script that lists all states from the database hbtn_0e_0_usa:
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-# Your script should take 3 arguments: mysql username, mysql password
-# and database name (no argument validation needed)
-# You must use the module MySQLdb (import MySQLdb)
-# Your script should connect to a MySQL server running on localhost at
-# port 3306
-# Results must be sorted in ascending order by states.id
-# Results must be displayed as they are in the example below
-# Your code should not be executed when imported
+    try:
+        # Connect to the MySQL server running on localhost at port 3306
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
 
-# guillaume@ubuntu:~/$ cat 0-select_states.sql
-# -- Create states table in hbtn_0e_0_usa with some data
-# CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-# USE hbtn_0e_0_usa;
-# CREATE TABLE IF NOT EXISTS states (
-#  id INT NOT NULL AUTO_INCREMENT,
-#  name VARCHAR(256) NOT NULL,
-# PRIMARY KEY (id)
-# );
-# INSERT INTO states (name) VALUES ("California"), ("Arizona"),
-# ("Texas"), ("New York"), ("Nevada");
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
 
-# guillaume@ubuntu:~/$ cat 0-select_states.sql | mysql -uroot -p
-# Enter password:
-# guillaume@ubuntu:~/$ ./0-select_states.py root root hbtn_0e_0_usa
-# (1, 'California')
-# (2, 'Arizona')
-# (3, 'Texas')
-# (4, 'New York')
-# (5, 'Nevada')
-# guillaume@ubuntu:~/$
+        # Execute the SQL query to retrieve states sorted by id
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-# No test cases needed
+        # Fetch all the rows and print the results
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
 
-# ////////////////////////////////////////////////////////////////
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
 
-"""lists all states from the database hbtn_0e_0_usa"""
+    finally:
+        # Close the database connection
+        if db:
+            db.close()
 
-if __name__ == '__main__':
-
-    import MySQLdb
-    import sys
-
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
