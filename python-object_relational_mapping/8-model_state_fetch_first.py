b3b002035fc69e71605e8c69aee5758653e4236d
Kahiga01 <danielkahiga69@gmail.com>
2023-09-09 09:15:47 -0700

    First state

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-This script prints the first State object
-from the database `hbtn_0e_6_usa`.
-"""
+# 8-model_state_fetch_first.py
+"""import a model and fitch the first row"""
 
 from sys import argv
 from model_state import Base, State
@@ -10,20 +8,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Access to the database and get a state
-    from the database.
-    """
 
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format
+        (argv[1],
+         argv[2],
+         argv[3]),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
     session = Session()
-    instance = session.query(State).order_by(State.id).first()
 
-    if instance is None:
-        print('Nothing')
-    else:
-        print('{0}: {1}'.format(instance.id, instance.name))
+    if session.query(State).count() == 0:
+        print("Nothing")
+
+    state = session.query(State).first()
+
+    print('{}: {}'.format(state.id, state.name))
+    session.close()
