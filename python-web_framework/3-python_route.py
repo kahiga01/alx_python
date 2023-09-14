307d5b00c89e2bf95fb2ca1f9b3c7fd32c157eb2
Kahiga01 <danielkahiga69@gmail.com>
2023-09-13 18:43:24 -0700

    python is cool

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -5,26 +5,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route("/", strict_slashes=False)
+def hello():
     return "Hello HBNB!"
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+@app.route("/hbnb", strict_slashes=False)
+def hbn():
     return "HBNB"
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    # Replace underscores with spaces
-    text = text.replace('_', ' ')
-    return "C {}".format(text)
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    return "C %s" % text
 
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text="is_cool"):
-    # Replace underscores with spaces
-    text = text.replace('_', ' ')
-    return "Python {}".format(text)
+@app.route("/python", strict_slashes=False)
+@app.route("/python/text/<int:text>", strict_slashes=False)
+def python(text = "is cool"):
+    return "Python_%s" % text
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host="0.0.0.0", port=5000)
