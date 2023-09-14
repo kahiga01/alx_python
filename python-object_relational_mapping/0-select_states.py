ee4ccdacccddb7c1651faf5052db264f519344e8
Kahiga01 <danielkahiga69@gmail.com>
2023-09-14 11:56:10 -0700

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,41 +1,16 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
+"""lists all states from the database hbtn_0e_0_usa"""
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+if __name__ == '__main__':
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    import MySQLdb
+    import sys
 
-    try:
-        # Connect to the MySQL server running on localhost at port 3306
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-        # Create a cursor object to interact with the database
-        cursor = db.cursor()
-
-        # Execute the SQL query to retrieve states sorted by id
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-        # Fetch all the rows and print the results
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    finally:
-        # Close the database connection
-        if db:
-            db.close()
-
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
